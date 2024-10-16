73816680928e5f88b31098d5061415e903e1429e
egbuniwecemeka <egbuniwecemeka.com>
2024-10-16 22:15:04 +0100

    We are all unique

---
+++ 0x00-MySQL_Advanced/0-unique_users.sql
@@ -0,0 +1,8 @@
+-- MySQL script that creates a table of users with id, email and name as attributes
+
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255),
+    PRIMARY KEY (`id`)
+);
