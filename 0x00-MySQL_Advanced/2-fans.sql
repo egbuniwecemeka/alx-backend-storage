0a7eee230c4bff53f2b5f2d461fac3b0d1d8d6a3
egbuniwecemeka <egbuniwecemeka.com>
2024-10-20 04:52:59 +0100

    Best band ever!

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- Ranks country origins, ordered by number of non-unique fans
 
-SELECT origin, SUM(fans) AS nb_fas
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fas DESC;
