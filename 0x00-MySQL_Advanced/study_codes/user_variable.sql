cda4c20143393d41cf8cf3c83f355a84d6401920
egbuniwecemeka <egbuniwecemeka.com>
2024-10-20 11:37:52 +0100

    User variables

---
+++ 0x00-MySQL_Advanced/study_codes/user_variable.sql
@@ -0,0 +1,15 @@
+-- A script show user variables in a procedure
+
+-- Set a temporal delimiter
+DELIMITER $$
+
+-- Create Procedure
+CREATE PROCEDURE user_var()
+    BEGIN
+        DECLARE x, y INT;
+        SET @x = 5;
+        SET @y = 10;
+        SELECT @x, @y, @x - @y; 
+    END
+
+DELIMETER ;
