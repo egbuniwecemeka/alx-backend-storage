120162630c024ac36177954a50c188a37b6945e1
egbuniwecemeka <egbuniwecemeka.com>
2024-10-22 01:27:38 +0100

    Email validation to sent.

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- SQL script creating a trigger that resets the valid_email attribute
+    -- only when the email nas been changed.
+DELIMITER $$
+
+-- Create Trigger that resets the valid_email on every email change
+CREATE TRIGGER reset_email
+AFTER UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET valid_email = 0
+        WHERE id = NEW.id;
+    END IF;
+END $$
