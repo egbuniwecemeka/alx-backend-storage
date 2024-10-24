b700940f3dad441930df20fd6f40f7d8ed0ff20a
egbuniwecemeka <egbuniwecemeka.com>
2024-10-24 14:40:16 +0100

    edit

--- 0x01-NoSQL/9-main.py
+++ 0x01-NoSQL/9-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ 9-main """
-list_all = __import__('8-list_all').list_all
+list_all = __import__('8-all').list_all
 insert_school = __import__('9-insert_school').insert_school
 
 from pymongo import MongoClient
