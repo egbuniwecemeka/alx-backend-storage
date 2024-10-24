0a45634432871025dd86ce0a2d849f34a02ee942
egbuniwecemeka <egbuniwecemeka.com>
2024-10-24 21:23:03 +0100

    Change school topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!?usr/bin/env python3
+""" A python script that changes all topics of a document based on the name """
+
+from typing import List
+
+def update_topics(mongo_collection, name: str, topics: List):
+    """ Updates topics of the school documents """
+    if mongo_collection != None:
+        # Use update_many to update multiple documents where name match
+        mongo_collection.update_many(
+        { 'name': name},
+        { '$set': {'topics': topics}}
+        )
