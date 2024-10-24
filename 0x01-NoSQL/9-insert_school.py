d4342f460a397f0779ee91949bf40ae875b4a273
egbuniwecemeka <egbuniwecemeka.com>
2024-10-24 14:34:56 +0100

    Insert a document in Python

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,11 +4,12 @@
 
 def insert_school(mongo_collection, **kwargs):
     """ Function inserts a document into a collection """
-    document = {}
     if mongo_collection != None:
         # Manually construct the document to insert into (Optional)
+        document = {}
         for key, value in kwargs.items():
             document[key] = value
+        # Insert the constructed document
         new_inserted_id = mongo_collection.insert_one(document)
         return new_inserted_id
     else:
